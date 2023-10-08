599031bdf3c125da7d754dd03dc59fe933086f5b
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-10-08 12:00:39 -0700

    made it dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayPassword : boolean = false;
+  timestamps : any = [];
+
+  onDisplayDetailsClick() {
+  this.displayPassword = !this.displayPassword;
+  this.timestamps.push(this.timestamps.length+1);
+  }
   }
